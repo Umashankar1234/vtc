3e4aa4d9414f3481083772276f6c0b09fcaca51d
uma shankar behera <cashdhell@gmail.com>
2023-09-12 20:27:58 +0530

    wfh

--- src/CommonMethods/Fetch.js
+++ src/CommonMethods/Fetch.js
@@ -7,7 +7,7 @@ export const APIURL = () => {
 }
 export const APIPath = () => {
   //  return "/";
-  return "/";
+  return "/alpha/";
 }
 // "homepage": "http://139.59.28.82/mybid_frontend/",
 // yarn add final-form react-final-form
